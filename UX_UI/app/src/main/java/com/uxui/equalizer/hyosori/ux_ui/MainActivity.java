4828ea3a9f9f7d58544f390c59e4ae4eef27a5df
JieunKim <laurelis37@gmail.com>
2017-07-25 18:16:36 +0900

    set tool bar

--- UX_UI/app/src/main/java/com/uxui/equalizer/hyosori/ux_ui/MainActivity.java
+++ UX_UI/app/src/main/java/com/uxui/equalizer/hyosori/ux_ui/MainActivity.java
@@ -4,12 +4,12 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         seekBars = (ConstraintLayout) findViewById(R.id.seekBarLayout);
 
@@ -68,13 +71,27 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
-        ImageButton moreMenuBtn = (ImageButton) findViewById(R.id.moreMenuBtn);
-        moreMenuBtn.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*VISIBLE*/
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_new) {
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
