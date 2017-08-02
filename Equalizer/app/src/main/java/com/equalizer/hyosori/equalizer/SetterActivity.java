f25e304ce345e5cb8b7c1fcfdf128c27c822cb85
JieunKim <laurelis37@gmail.com>
2017-08-03 01:25:50 +0900

    create android project for merging

---
+++ Equalizer/app/src/main/java/com/equalizer/hyosori/equalizer/SetterActivity.java
@@ -0,0 +1,13 @@
+package com.equalizer.hyosori.equalizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setter);
+    }
+}
