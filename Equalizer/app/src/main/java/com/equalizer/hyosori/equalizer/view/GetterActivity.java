9182fedba3b5c06d9c1b7ed91f20448096960008
JieunKim <laurelis37@gmail.com>
2017-08-23 20:20:19 +0900

    merge UI/UX

--- Equalizer/app/src/main/java/com/equalizer/hyosori/equalizer/view/GetterActivity.java
+++ Equalizer/app/src/main/java/com/equalizer/hyosori/equalizer/view/GetterActivity.java
@@ -1,7 +1,144 @@
 package com.equalizer.hyosori.equalizer.view;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.equalizer.hyosori.equalizer.R;
 
 
 public class GetterActivity extends Activity implements GetterView {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.getter);
+
+        final EditText editText = (EditText)findViewById(R.id.editText_name);
+
+
+        final TextView textView1 = (TextView) findViewById(R.id.text_view_state_result1);
+        final TextView textView2 = (TextView) findViewById(R.id.text_view_state_result2);
+        final TextView textView3 = (TextView) findViewById(R.id.text_view_state_result3);
+        final TextView textView4 = (TextView) findViewById(R.id.text_view_state_result4);
+        final TextView textView5 = (TextView) findViewById(R.id.text_view_state_result5);
+
+        Button button1 = (Button) findViewById(R.id.button_1);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        Button button3 = (Button) findViewById(R.id.button_3);
+        Button button4 = (Button) findViewById(R.id.button_4);
+        Button button5 = (Button) findViewById(R.id.button_5);
+
+        final Button R_button = (Button) findViewById(R.id.button_register);
+        R_button.setEnabled(false);
+
+        button1.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView1.setText("측정");
+
+                if(textView1.getText().toString().equals("측정") && textView2.getText().toString().equals("측정")
+                        && textView3.getText().toString().equals("측정") && textView4.getText().toString().equals("측정")
+                        && textView5.getText().toString().equals("측정")){
+                    if(editText.getText().toString().length() != 0){
+                        R_button.setEnabled(true);
+                    }
+                }
+
+            }
+        });
+
+        button2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView2.setText("측정");
+
+                if(textView1.getText().toString().equals("측정") && textView2.getText().toString().equals("측정")
+                        && textView3.getText().toString().equals("측정") && textView4.getText().toString().equals("측정")
+                        && textView5.getText().toString().equals("측정")){
+                    if(editText.getText().toString().length() != 0){
+                        R_button.setEnabled(true);
+                    }
+                }
+            }
+
+        });
+
+        button3.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView3.setText("측정");
+
+                if(textView1.getText().toString().equals("측정") && textView2.getText().toString().equals("측정")
+                        && textView3.getText().toString().equals("측정") && textView4.getText().toString().equals("측정")
+                        && textView5.getText().toString().equals("측정")){
+                    if(editText.getText().toString().length() != 0){
+                        R_button.setEnabled(true);
+                    }
+                }
+            }
+        });
+
+        button4.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView4.setText("측정");
+
+                if(textView1.getText().toString().equals("측정") && textView2.getText().toString().equals("측정")
+                        && textView3.getText().toString().equals("측정") && textView4.getText().toString().equals("측정")
+                        && textView5.getText().toString().equals("측정")){
+                    if(editText.getText().toString().length() != 0){
+                        R_button.setEnabled(true);
+                    }
+                }
+            }
+        });
+
+        button5.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView5.setText("측정");
+
+                if(textView1.getText().toString().equals("측정") && textView2.getText().toString().equals("측정")
+                        && textView3.getText().toString().equals("측정") && textView4.getText().toString().equals("측정")
+                        && textView5.getText().toString().equals("측정")){
+                    if(editText.getText().toString().length() != 0){
+                        R_button.setEnabled(true);
+                    }
+                }
+            }
+        });
+
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(keyCode == event.KEYCODE_ENTER){
+//                    String name = editText.getText().toString();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(editText.getWindowToken(),0);
+                    String name = editText.getText().toString();
+                    if(name.length() == 0){
+                        R_button.setEnabled(false);
+                    } else{
+                        if(textView1.getText().toString().equals("측정") && textView2.getText().toString().equals("측정")
+                                && textView3.getText().toString().equals("측정") && textView4.getText().toString().equals("측정")
+                                && textView5.getText().toString().equals("측정")) {
+                            R_button.setEnabled(true);
+                        }
+                    }
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+
+
+    }
 }
--- Equalizer/app/src/main/java/com/equalizer/hyosori/equalizer/view/SetterActivity.java
+++ Equalizer/app/src/main/java/com/equalizer/hyosori/equalizer/view/SetterActivity.java
@@ -1,16 +1,100 @@
 package com.equalizer.hyosori.equalizer.view;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.equalizer.hyosori.equalizer.R;
 
 
 public class SetterActivity extends AppCompatActivity implements SetterView {
+    private ConstraintLayout seekBars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        seekBars = (ConstraintLayout) findViewById(R.id.seekBarLayout);
+
+        Button applyBtn = (Button) findViewById(R.id.btn_apply);
+        applyBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final int childCount = seekBars.getChildCount();
+                for (int i = 0; i < childCount; ++i) {
+                    View v = seekBars.getChildAt(i);
+                    v.setEnabled(true);
+                }
+            }
+        });
+
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                final int childCount = seekBars.getChildCount();
+                for (int i = 0; i < childCount; ++i) {
+                    View v = seekBars.getChildAt(i);
+                    v.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                final int childCount = seekBars.getChildCount();
+                for (int i = 0; i < childCount; ++i) {
+                    View v = seekBars.getChildAt(i);
+                    v.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_new) {
+            startActivity(new Intent(SetterActivity.this, GetterActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
